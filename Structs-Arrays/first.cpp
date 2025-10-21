5144c00555d0ac9c28bcc0163781eca197c93513
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-21 10:05:35 +0600

    first commit

---
+++ Structs-Arrays/first.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // --- your code here ---
+    cout << "Hello world" << endl;
+    return 0;
+}
