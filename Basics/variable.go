6d960f7d61cec8600ec3c8de62b15828055618d3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-23 09:23:59 +0600

    added

--- Basics/variable.go
+++ Basics/variable.go
@@ -1,8 +1,13 @@
-package main 
+package main
+
+import "fmt"
 
 func main() {
-   // Start your code....
+	var student1 string = "John" //type is string
+	var student2 = "Jane"        //type is inferred
+	x := 2                       //type is inferred
 
-    //
-   
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
 }
