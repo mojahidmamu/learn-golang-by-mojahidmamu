bab64a9197c0c371a3961452b104e2882f54322d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-23 09:47:54 +0600

    added

--- Basics/formating_verb.go
+++ Basics/formating_verb.go
@@ -17,4 +17,18 @@ func main() {
 	fmt.Printf("%#v\n", txt)
 	fmt.Printf("%T\n", txt)
 
+	var age = 15
+
+	fmt.Printf("%b\n", age)
+	fmt.Printf("%d\n", age)
+	fmt.Printf("%+d\n", age)
+	fmt.Printf("%o\n", age)
+	fmt.Printf("%O\n", age)
+	fmt.Printf("%x\n", age)
+	fmt.Printf("%X\n", age)
+	fmt.Printf("%#x\n", age)
+	fmt.Printf("%4d\n", age)
+	fmt.Printf("%-4d\n", age)
+	fmt.Printf("%04d\n", age)
+
 }
