c2686768e7ecda424ce5fd15ae3e16bc852438fd
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-23 09:27:57 +0600

    added

--- Basics/Multiple Variable Declaration.go
+++ Basics/Multiple Variable Declaration.go
@@ -1,7 +1,12 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   fmt.Println("Hello World!")
+	// Start your code....
+	var a, b, c, d int = 2, 5, 3, 7
+	fmt.Println(a);
+	fmt.Println(b);
+	fmt.Println(c);
+	fmt.Println(d);
 }
