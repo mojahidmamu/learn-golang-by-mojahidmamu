eafaa63fea89f5d97d93fc55f2420b2cfee3e9ba
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-24 12:50:45 +0600

    added

--- Arrays, Condition & Loop/arrray.go
+++ Arrays, Condition & Loop/arrray.go
@@ -7,4 +7,10 @@ func main() {
    // Array defination: 
    //    => Arrays are used to store multiple values of the same type in a single variable,
    //       instead of declaring separate variables for each value.
+
+   var arr1 = [3] int{1,2,3};
+   arr2 := [5] int{1,2,3,5,8};
+
+   fmt.Println(arr1);
+   fmt.Println(arr2);
 }
