31b3157813b8021ed9b341f94a9d6dbf46a9e72e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-24 12:53:55 +0600

    added

--- Arrays, Condition & Loop/arrray.go
+++ Arrays, Condition & Loop/arrray.go
@@ -1,16 +1,24 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   
-   // Array defination: 
-   //    => Arrays are used to store multiple values of the same type in a single variable,
-   //       instead of declaring separate variables for each value.
+	// Start your code....
 
-   var arr1 = [3] int{1,2,3};
-   arr2 := [5] int{1,2,3,5,8};
+	// Array defination:
+	//    => Arrays are used to store multiple values of the same type in a single variable,
+	//       instead of declaring separate variables for each value.
 
-   fmt.Println(arr1);
-   fmt.Println(arr2);
+	/*
+	   Syntax :
+         var array_name = [array_length] data-type{values}
+	*/
+
+	var arr1 = [3]int{1, 2, 3} // data-type : int 
+	arr2 := [5]int{1, 2, 3, 5, 8}
+   var cars = [4] string{"Volvo", "BMW", "Ford", "Mazda"}; // data-type : string
+
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+   fmt.Println(cars);
 }
