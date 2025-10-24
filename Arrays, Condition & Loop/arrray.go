a50cd7e6c8a4f6fca6f406385314e74a9acc1c9d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-24 12:40:20 +0600

    added

--- Arrays, Condition & Loop/arrray.go
+++ Arrays, Condition & Loop/arrray.go
@@ -3,5 +3,8 @@ import "fmt"
 
 func main() {
    // Start your code....
-   fmt.Println("Hello World!")
+   
+   // Array defination: 
+   //    => Arrays are used to store multiple values of the same type in a single variable,
+   //       instead of declaring separate variables for each value.
 }
