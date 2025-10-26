4f57af6ff98f1b8c53d1aa0e833a4efc7e3e43ca
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-26 08:32:42 +0600

    added

--- Function, Map, Struct/Function.go
+++ Function, Map, Struct/Function.go
@@ -1,7 +1,11 @@
 package main
 import "fmt"
 
+func message() {
+	fmt.Println("Abdullah all MOjahid")
+}
+
 func main() {
    // Start your code....
-    
+    message(); // call the function ... 
 }
