0d8757b8103013f82488118cfac0a6097e01c3c6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-26 07:40:04 +0600

    added

--- Arrays, Condition & Loop/Condition.go
+++ Arrays, Condition & Loop/Condition.go
@@ -4,12 +4,26 @@ import "fmt"
 
 func main() {
 	// Start your code....
-	var time = 22
-	if time < 10 {
-		fmt.Println("Good morning.")
-	} else if time < 20 {
-		fmt.Println("Good day.")
+
+	// if-else condition:
+	// var time = 22
+	// if time < 10 {
+	// 	fmt.Println("Good morning.")
+	// } else if time < 20 {
+	// 	fmt.Println("Good day.")
+	// } else {
+	// 	fmt.Println("Good evening.")
+	// }
+
+	// nested if-else conditoin:
+	var num = 20
+	if num >= 10 {
+		fmt.Println("Num is more than 10.")
+		if num > 15 {
+			fmt.Println("Num is also more than 15.")
+		}
 	} else {
-		fmt.Println("Good evening.")
+		fmt.Println("Num is less than 10.")
 	}
+
 }
