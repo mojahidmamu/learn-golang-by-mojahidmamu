75cc40e4da3b1893d181cc639278b770f5111cd1
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-26 08:31:14 +0600

    added

--- Arrays, Condition & Loop/Loop.go
+++ Arrays, Condition & Loop/Loop.go
@@ -6,14 +6,14 @@ func main() {
 	// Start your code....
 	for i := 0; i < 5; i++ {
 		if i == 3 {
-			break;
+			break; // break;
 		}
 		fmt.Println(i)
 	}
 
 	for i := 0; i <= 100; i += 10 {
 		if i == 50 {
-			continue
+			continue // continue
 		}
 		fmt.Println(i)
 	}
