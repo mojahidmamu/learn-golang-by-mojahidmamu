417c9c585c94bdda8b93de6a3210ab298087c047
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-26 08:38:01 +0600

    added

--- Function, Map, Struct/Function.go
+++ Function, Map, Struct/Function.go
@@ -1,11 +1,23 @@
 package main
+
 import "fmt"
 
 func message() {
 	fmt.Println("Abdullah all MOjahid")
 }
 
+func familyName(siblingName string, age int) {
+	fmt.Println("Hello" , siblingName, "bin Yousouf and your are", age, "Years old" )
+}
+
 func main() {
-   // Start your code....
-    message(); // call the function ... 
+	// Start your code....
+	message() // call the function ...
+	message()
+	message()
+
+	// 
+	familyName("Salma Tasnim", 20)
+	familyName("Abdullah all Mojahid",18)
+	familyName("Halimatus Sadia",10)
 }
