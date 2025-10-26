09af3c1fa9c943e5424435187cec80f5bfa04d81
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-26 09:11:10 +0600

    added

--- Function, Map, Struct/Recursion.go
+++ Function, Map, Struct/Recursion.go
@@ -10,7 +10,15 @@ func testCount(x int) int {
 	return testCount(x + 1)
 }
 
+func factorial(x int) int {
+	if x == 0 {
+		return 1
+	}
+	return x * factorial(x-1)
+}
+
 func main() {
 	// Start your code....
 	testCount(1)
+	fmt.Println(factorial(4))
 }
