c98a0f588bec130502c6d26948525b2681cfe3f9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-26 09:06:43 +0600

    added

--- Function, Map, Struct/Recursion.go
+++ Function, Map, Struct/Recursion.go
@@ -1,7 +1,16 @@
 package main
+
 import "fmt"
 
+func testCount(x int) int {
+	if x == 11 {
+		return 0
+	}
+	fmt.Println(x)
+	return testCount(x + 1)
+}
+
 func main() {
-   // Start your code....
-   fmt.Println("Hello World!")
+	// Start your code....
+	testCount(1)
 }
