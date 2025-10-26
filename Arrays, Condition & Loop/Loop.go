a66b2bf5ff11d7fa5b65b68ab533a0a6cf2cb43c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-26 08:29:31 +0600

    added

--- Arrays, Condition & Loop/Loop.go
+++ Arrays, Condition & Loop/Loop.go
@@ -1,9 +1,20 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-    for i := 0; i < 5; i++ {
+	// Start your code....
+	for i := 0; i < 5; i++ {
+		if i == 3 {
+			break;
+		}
+		fmt.Println(i)
+	}
+
+	for i := 0; i <= 100; i += 10 {
+		if i == 50 {
+			continue
+		}
 		fmt.Println(i)
 	}
 }
