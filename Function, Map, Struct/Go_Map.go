07960fab0d2c265472e94d688c715b70d7014b90
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-26 09:43:09 +0600

    added

--- Function, Map, Struct/Go_Map.go
+++ Function, Map, Struct/Go_Map.go
@@ -1,7 +1,17 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   fmt.Println("Hello World!")
+	// Start your code....
+	var a = make(map[string]string)
+	a["brand"] = "Ford"
+	a["model"] = "Mustang"
+	a["year"] = "1964"
+
+	fmt.Println(a)
+
+	a["year"] = "2025"   // update element
+	a["color"] = "Black" // adding element
+	fmt.Println(a)
 }
