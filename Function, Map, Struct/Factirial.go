a0ee683d7cdbf85359d2773edd430d2749be4ea9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-31 07:59:13 +0600

    added

---
+++ Function, Map, Struct/Factirial.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func factorial(a int) int {
+	if a == 0 {
+		return 1
+	}
+	return a * factorial(a-1)
+}
+
+func main() {
+	// Start your code....
+	fmt.Print(factorial(5))
+}
