9f3a446e18c1f14ae6644e184946c51af6879f2a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-10-31 07:54:15 +0600

    added

--- Function, Map, Struct/init_function.go
+++ Function, Map, Struct/init_function.go
@@ -1,7 +1,12 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   fmt.Println("Hello World!")
+	// Start your code....
+	fmt.Println("Hello World!")
+}
+
+func init() {
+	fmt.Println("Hello World!")
 }
