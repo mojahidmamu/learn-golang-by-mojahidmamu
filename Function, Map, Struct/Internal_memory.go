697ada63aaf81666b871cf8b42d631dac29af3db
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-03 19:46:20 +0600

    added

--- Function, Map, Struct/Internal_memory.go
+++ Function, Map, Struct/Internal_memory.go
@@ -1,21 +1,21 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   
-   
-   // Internal memory example
-   type Person struct {
-	   Name string
-	   Age  int
-   }
-   p1 := Person{Name: "Alice", Age: 30}
-   p2 := p1 // p2 is a copy of p1
-   p2.Age = 31
+	// Start your code....
 
-   fmt.Println("p1 Age:", p1.Age)
-   fmt.Println("p2 Age:", p2.Age)
-   
-   // End your code....
+	// Internal memory example
+	type Person struct {
+		Name string
+		Age  int
+	}
+	p1 := Person{Name: "Alice", Age: 30}
+	p2 := p1 // p2 is a copy of p1
+	p2.Age = 31
+
+	fmt.Println("p1 Age:", p1.Age)
+	fmt.Println("p2 Age:", p2.Age)
+
+	// End your code....
 }
