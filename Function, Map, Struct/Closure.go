b4d1d1b9bd34508dc840591049185226f732dbb0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-04 07:57:44 +0600

    added

--- Function, Map, Struct/Closure.go
+++ Function, Map, Struct/Closure.go
@@ -1,7 +1,19 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   fmt.Println("Hello World!")
+	// Start your code....
+	// Closure example
+	counter := func() func() int {
+		count := 0
+		return func() int {
+			count++
+			return count
+		}
+	}()
+	fmt.Println(counter()) // Output: 1
+	fmt.Println(counter()) // Output: 2
+	fmt.Println(counter()) // Output: 3
+	// End your code....
 }
