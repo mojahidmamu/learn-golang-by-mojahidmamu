f2eeda09980f7d17a87e78599253e27caae52ed7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-04 10:30:17 +0600

    added

--- Function, Map, Struct/Function_Factory.go
+++ Function, Map, Struct/Function_Factory.go
@@ -1,7 +1,18 @@
 package main
+
 import "fmt"
 
+func multiple(a int) func(int) int {
+	return func(b int) int {
+		return a * b
+	}
+}
+
 func main() {
-   // Start your code....
-   fmt.Println("Hello World!")
+	// Start your code....
+	double := multiple(2)
+	times3 := multiple(3)
+	result := times3(4)
+	fmt.Println(double(5)) // Output: 10
+	fmt.Println(result)     // Output: 12
 }
