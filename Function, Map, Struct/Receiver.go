7084aae698c9870f4de1774aa1d1b907259b7c44
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-08 09:24:52 +0600

    clousure

--- Function, Map, Struct/Receiver.go
+++ Function, Map, Struct/Receiver.go
@@ -1,17 +1,22 @@
 package main
+
 import "fmt"
 
-type Person struct {
+type Person struct { // Struct definition
 	Name string
 	Age  int
-	Email string
+}
+
+func (p Person) Greet() { // Receiver function
+	fmt.Printf("Hello, my name is %s. I am %d years old. \n", p.Name, p.Age)
 }
 
 func main() {
-   // Start your code....
-    p1 := Person{Name: "Mojahid", Age: 24, Email: "abdullah@example.com"}
-   fmt.Println(p1)
-   fmt.Println(p1.Name)
-   fmt.Println(p1.Age)
-   fmt.Println(p1.Email)
+	// Start your code....
+	p1 := Person{Name: "Mojahid", Age: 24} // Struct initialization
+	p1.Greet()
+	// fmt.Println(p1)
+	// fmt.Println(p1.Name)
+	// fmt.Println(p1.Age)
+
 }
