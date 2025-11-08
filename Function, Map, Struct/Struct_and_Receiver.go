fe278fcb3de5d2d17265062e1503acb0449d5261
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-08 09:58:55 +0600

    clousure

--- Function, Map, Struct/Struct_and_Receiver.go
+++ Function, Map, Struct/Struct_and_Receiver.go
@@ -11,13 +11,13 @@ func (b *BankAccount) Diposit(amount float64) { // Receiver function with pointe
 	b.balance += amount
 }
 
-func (b *BankAccount) showBalance() { // Receiver function to show balance	
+func (b *BankAccount) showBalance() { // Receiver function to show balance
 	fmt.Printf("Account owner: %s, Balance: $%.2f\n", b.owner, b.balance)
 }
 
-func main() { 
+func main() {
 	// Start your code....
 	account := BankAccount{owner: "Mojahid", balance: 1000.00} // Struct initialization
-	account.Diposit(500.00)
-	account.showBalance()
+	account.Diposit(500.00)                                    // Deposit money
+	account.showBalance()                                      // Show balance
 }
