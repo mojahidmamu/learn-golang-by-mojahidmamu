960edca8f342d48332f3003266c1913a64be77aa
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-08 09:20:29 +0600

    clousure

--- Function, Map, Struct/Struct.go
+++ Function, Map, Struct/Struct.go
@@ -1,7 +1,17 @@
 package main
 import "fmt"
 
+type Person struct {
+	Name string
+	Age  int
+	Email string
+}
+
 func main() {
    // Start your code....
-   fmt.Println("Hello World!")
+    p1 := Person{Name: "Abdullah all Mojahid", Age: 24, Email: "abdullah@example.com"}
+   fmt.Println(p1)
+   fmt.Println(p1.Name)
+   fmt.Println(p1.Age)
+   fmt.Println(p1.Email)
 }
