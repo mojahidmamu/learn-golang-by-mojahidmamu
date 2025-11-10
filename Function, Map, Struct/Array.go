a681c546beaf142136b5ae320ba460938f06ca56
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-10 17:00:33 +0600

    clousure

--- Function, Map, Struct/Array.go
+++ Function, Map, Struct/Array.go
@@ -4,10 +4,12 @@ import "fmt"
 
 func main() {
 	// Start your code....
-	arr := []int{1, 2, 3, 4, 5}
+	arr := [5]int{1, 2}
 	sum := 0
 	for _, num := range arr {
 		sum += num
 	}
 	fmt.Println("Sum:", sum)
+	fmt.Println(arr)
+	// End your code.
 }
