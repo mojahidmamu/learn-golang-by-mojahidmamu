08b2cf3190c0f4a4cb8146d69b9a0a6b540882c9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-23 21:31:09 +0600

    added

--- Pointer/main.go
+++ Pointer/main.go
@@ -7,6 +7,7 @@ func main() {
 	x := 20 
 	a := &x // address of x 
 	*a = 50 // reassign value of address a	
+	
 	fmt.Println("This is value", x)
 	fmt.Println("This is address", a)
 	fmt.Println("value of address", *a)
