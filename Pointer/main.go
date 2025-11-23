0d915dd258bf21d245372f50fcf2810e3ccd7430
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-23 20:48:06 +0600

    added

--- Pointer/main.go
+++ Pointer/main.go
@@ -4,9 +4,12 @@ import "fmt"
 
 func main() {
 	// Start your code....
-	x := 20
-	a := &x
+	x := 20 
+	a := &x // address of x 
+	*a = 50 // reassign value of address a	
 	fmt.Println("This is value", x)
 	fmt.Println("This is address", a)
 	fmt.Println("value of address", *a)
+	fmt.Println("reassign value", a)
+	fmt.Println("reassign value", *a)
 }
