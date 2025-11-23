18bc763b9c8930d85df62bb5b656e990c2c651ef
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-23 20:34:10 +0600

    added

--- Pointer/main.go
+++ Pointer/main.go
@@ -1,8 +1,12 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   x := 20
-   fmt.Println(x)
+	// Start your code....
+	x := 20
+	a := &x
+	fmt.Println("This is value", x)
+	fmt.Println("This is address", a)
+	fmt.Println("value of address", *a)
 }
