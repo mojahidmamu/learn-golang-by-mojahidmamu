934236fa63454e825a95139f716438e833f395fc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 11:28:09 +0600

    added

--- Slice/Slice__share.go
+++ Slice/Slice__share.go
@@ -1,7 +1,11 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   fmt.Println("Hello World!")
+	// Start your code....
+	arr := [5]int{10, 20, 30, 40, 50}
+	s1 := arr[1:3]
+	s2 := arr[2:]
+	fmt.Println("Before", s1, s2)
 }
