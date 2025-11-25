622c47d0ed7fa7b223c4a7c86ab0d328df539e2f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 19:20:00 +0600

    added

--- Slice/re-slicing.go
+++ Slice/re-slicing.go
@@ -6,8 +6,9 @@ func main() {
 	// Start your code....
 	s1 := []int{1, 2, 3, 4, 5}
 
-	a := s1[1:3]
-	// b := s1[2:]
+	a := s1[1:3] //[2 3]
+	b := s1[2:]  //[3 4 5]
 
-	fmt.Println(a, len(a), cap(a))
+	fmt.Println(a, "length", len(a), "capacity", cap(a))
+	fmt.Println(b, "length", len(b), "capacity", cap(b))
 }
