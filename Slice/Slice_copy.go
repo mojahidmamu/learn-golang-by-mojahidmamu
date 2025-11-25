44ded5c7df44acdf99a2e314f43ec930230f8182
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 19:15:04 +0600

    added

--- Slice/Slice_copy.go
+++ Slice/Slice_copy.go
@@ -1,7 +1,13 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   fmt.Println("Hello World!")
+	// Start your code....
+	s1 := []int{1, 2, 3, 4, 5}
+	s2 := make([]int, len(s1))
+	copy(s2, s1)
+
+	fmt.Println("s1:", s1) // [1 2 3]
+	fmt.Println("s2:", s2) // [999 2 3]
 }
