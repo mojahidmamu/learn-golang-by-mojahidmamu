7e38fcf3cf0d27712a27e94d92c55a5d11ee0a52
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 19:30:14 +0600

    added

--- Slice/Dynamic_list_as_slice.go
+++ Slice/Dynamic_list_as_slice.go
@@ -1,7 +1,39 @@
 package main
-import "fmt"
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-   // Start your code....
-    
+	// Start your code....
+	var numbers []int
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("enter numbers (type 'end' to stop)")
+
+	for {
+		fmt.Println("> ")
+		if !scanner.Scan() {
+			break
+		}
+
+		text := scanner.Text()
+		if text == "end" {
+			break
+		}
+
+		var x int
+		_, err := fmt.Sscanf(text, "%d", &x)
+
+		if err == nil {
+			fmt.Println("")
+			continue
+		}
+
+		numbers = append(numbers, x)
+	}
+	fmt.Println("You entered :", numbers)
+
 }
