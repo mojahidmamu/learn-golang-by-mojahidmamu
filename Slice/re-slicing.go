3f5a3182abe58f8d6c433951f398b1cb84990bbc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 19:18:36 +0600

    added

--- Slice/re-slicing.go
+++ Slice/re-slicing.go
@@ -1,7 +1,13 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   fmt.Println("Hello World!")
+	// Start your code....
+	s1 := []int{1, 2, 3, 4, 5}
+
+	a := s1[1:3]
+	// b := s1[2:]
+
+	fmt.Println(a, len(a), cap(a))
 }
