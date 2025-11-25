5c5f012cdea771d78b55f6943666d41d5a5a95f3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 11:15:58 +0600

    added

--- Slice/SliceWithMake.go
+++ Slice/SliceWithMake.go
@@ -1,13 +1,12 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-   arr := [5]int{10,20,30,40,50}
+	// Start your code....
+	s := make([]int, 3, 5) // make(type, length, capacity)
 
-   s1 := arr[1:3]
-   fmt.Println("S1 = " , s1)
-   fmt.Println("S1 = " , s1[len(s1)-1])
-   fmt.Println("Len of s1 = " , len(s1))
-   fmt.Println("cap of s1 = " , cap(s1))
+	fmt.Println("S1 = ", s)
+	fmt.Println("Len of s = ", len(s))
+	fmt.Println("cap of s = ", cap(s))
 }
