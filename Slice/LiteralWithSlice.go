24fc27160fd17564cbdf23e962ff0cafe1c93b93
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 11:19:04 +0600

    added

---
+++ Slice/LiteralWithSlice.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Start your code....
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(nums)      // [1 2 3 4]
+	fmt.Println(len(nums)) // 4
+	fmt.Println(cap(nums)) // 4
+
+}
