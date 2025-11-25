385a78c158d58563a791e65ab3137e94393ed2bb
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-25 11:25:37 +0600

    added

--- Slice/Append_in_slice.go
+++ Slice/Append_in_slice.go
@@ -1,8 +1,12 @@
 package main
+
 import "fmt"
 
 func main() {
-   // Start your code....
-    arr := [5]int{10,20,30,40,50}
-	fmt.Println(arr, len(arr), cap(arr))
+	// Start your code....
+	nums := []int{10, 20, 30, 40, 50}
+	fmt.Println(nums, len(nums), cap(nums))
+
+	nums = append(nums, 4, 100, 200)
+	fmt.Println(nums, len(nums), cap(nums))
 }
