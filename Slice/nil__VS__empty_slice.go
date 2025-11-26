6dfbd39cfde345fa95defe963664fe359658ecfd
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-26 09:53:55 +0600

    added

--- Slice/nil__VS__empty_slice.go
+++ Slice/nil__VS__empty_slice.go
@@ -4,11 +4,12 @@ import "fmt"
 
 func main() {
 	// Start your code....
-	var a []int  //nil slice
+	var a []int  // nil slice
 	b := []int{} // empty slice
 
 	fmt.Println(a == nil)       // true
 	fmt.Println(b == nil)       // false
 	fmt.Println(len(a), len(b)) // 0 0
+	fmt.Println(a, b)           // [] []
 
 }
