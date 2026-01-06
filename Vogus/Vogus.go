30d94068229355224ab02728596ad0e8026046eb
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-06 15:52:41 +0600

    added

--- Vogus/Vogus.go
+++ Vogus/Vogus.go
@@ -6,8 +6,9 @@ func main() {
    var a int8 = 42 // -128 to 127
    var b uint8 = 20 // 0 to 255
    
+   var c float32 = 3.14 // 32-bit floating point
+   var d float64 = 3.14159265359 // 64-bit floating point
 
-    
-   fmt.Println(a, b)
-    
+   fmt.Println(a, b, c, d)
+
 }
