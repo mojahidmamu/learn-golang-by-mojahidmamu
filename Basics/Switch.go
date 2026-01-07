5b5d177e47e816b249d8893ec60eb5e0c2a25fae
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-07 10:53:27 +0600

    added

---
+++ Basics/Switch.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Start your code....
+	i := 2
+	switch i {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	default:
+		fmt.Println("Other number")
+	}
+}
