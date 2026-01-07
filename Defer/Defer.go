fb35f6075712c11d7ca9fae24079a9b1e756cde9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-07 10:20:23 +0600

    added

--- Defer/Defer.go
+++ Defer/Defer.go
@@ -1,7 +1,15 @@
 package main
 import "fmt"
 
+func a () {
+	i := 0
+	defer fmt.Println("defer:", i)
+	i++
+	fmt.Println("i:", i)
+
+}
+
 func main() {
    // Start your code....
-   fmt.Println("Hello World!")
+   a()
 }
