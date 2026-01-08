9ce14dd4bce95c936f22a56c7bbe5f90ceb1c2c7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-01-08 10:18:46 +0600

    added

--- Defer/Defer+Panic.go
+++ Defer/Defer+Panic.go
@@ -4,12 +4,13 @@ import "fmt"
 
 func main() {
 	// Start your code....
-	great := func(name string) {
+	great := func(name string) { // Anonymous function assigned to variable
 		fmt.Println("Hello", name)
 	}
 
-	great("Alice")
+	great("Alice") // Call the anonymous function with argument
 
+	// Demonstrate defer and panic
 	defer fmt.Println("Clean up") // This will execute when the function exits, even if there's a panic
 	panic("Something wrong")      // This will trigger a panic
 
